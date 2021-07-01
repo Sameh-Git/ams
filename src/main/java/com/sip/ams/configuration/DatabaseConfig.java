c1e37efad0d261bd41c2e6aadbb220ac0a010a01
Sameh-Git <sameh.trabelsii92@gmail.com>
2021-07-01 10:29:39 +0100

    update bootstrap Article + Provider

--- src/main/java/com/sip/ams/configuration/DatabaseConfig.java
+++ src/main/java/com/sip/ams/configuration/DatabaseConfig.java
@@ -5,7 +5,7 @@ import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
-@Configuration
+/*@Configuration
 public class DatabaseConfig {
 @Value("${spring.datasource.url}")
 private String dbUrl;
@@ -15,4 +15,4 @@ HikariConfig config = new HikariConfig();
 config.setJdbcUrl(dbUrl);
 return new HikariDataSource(config);
 }
-}
+}*/
